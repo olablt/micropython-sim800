39713f16d4ee82b5c2609d69d3e3e3a8acc7b9b2
Nemo-nb <melitonas@gmail.com>
2018-04-04 16:48:07 +0300

    cleanup

--- class_sim800.py
+++ class_sim800.py
@@ -66,79 +66,6 @@ class sim_uart():
 		result = self.result
 		return result
 
-
-async def app_loop():
-	
-	print("sim = delay_uart.init...")
-	sim = delay_uart()
-	
-	print("sim.command...")
-	await sim.command("AT+CGMI")
-	await sim.command("ATZ", "OK")
-	# await sim.command("ATE0", "OK") # disable echo
-	await sim.command('AT+CMEE=2', "OK") # enable error strings
-	await sim.command("AT", "OK")
-	await sim.command("AT+CBC", "+CBC")
-	
-	apn = "internet.tele2.lt"
-	url = 'http://exploreembedded.com/wiki/images/1/15/Hello.txt'
-	# res = await sim.command('AT+CREG?') # Check the Network Registration?
-	
-	res = await sim.command('AT+SAPBR=3,1,"Contype","GPRS"', 'OK') # connection type
-	res = await sim.command('AT+SAPBR=3,1,"APN","{}"'.format(apn), 'OK') # GPRS APN
-	
-	res = await sim.command('AT+SAPBR=2,1') # Get current bearer
-	# If bearer == 0,0,0,0 (not assigned) get a new bearer
-	#  +SAPBR: 1,3,"0.0.0.0"
-	
-	res = await sim.command('AT+SAPBR=1,1', 'OK') # Open GPRS connection on profile 1
-	res = await sim.command('AT+HTTPINIT', 'OK') # Init HTTP service
-	res = await sim.command('AT+HTTPPARA="CID",1', 'OK') # Choose profile 1 as HTTP channel
-	res = await sim.command('AT+HTTPPARA="URL","{}"'.format(url), 'OK')
-	# res = await sim.command('AT+HTTPPARA="REDIR",1', 'OK')
-	res = await sim.command('AT+HTTPACTION=0', '+HTTPACTION') # start GET session (may be long)
-	res = await sim.command('AT+HTTPREAD') # Read the data of HTTP server
-	res = await sim.command('AT+HTTPTERM', 'OK') # terminate HTTP task
-	res = await sim.command('AT+SAPBR=0,1', 'OK') # close Bearer context
-	
-	print("done.")
-	
-	# while True:
-	# 	# print( "battery: ", await get_battery() )
-	# 	# print( "http: ", await get_http('http://exploreembedded.com/wiki/images/1/15/Hello.txt') )
-	# 	await asyncio.sleep(5)
-
-
-def run():
-	print("loop.run_forever...")
-	loop = asyncio.get_event_loop()
-	loop.create_task(app_loop())
-	loop.run_forever()
-
-
-
-
-
-
-# async def get_battery():   
-# 	result = await command('AT+CBC')
-# 	if result:
-# 		params=result[0].split(',')
-# 		if not params[0] == '':
-# 			params2 = params[0].split(':')
-# 			if params2[0]=='+CBC':
-# 				return int(params[1])
-# 	return 0
-	
-# async def get_http(url, apn="internet.tele2.lt"): 
-
-
-
-
-
-
-
-
 def convert_to_string(buf):
 	try:
 		tt =  buf.decode('utf-8').strip()
@@ -149,5 +76,3 @@ def convert_to_string(buf):
 			if tmp[i]>127:
 				tmp[i] = ord('#')
 		return bytes(tmp).decode('utf-8').strip()
-
-
